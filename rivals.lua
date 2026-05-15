06c8c679bd9cc5036f9d34709dcdf63b19fd4da7
voidedryann-droid <voidedryann@gmail.com>
2026-05-15 12:55:52 -0500

    Update rivals.lua

--- rivals.lua
+++ rivals.lua
@@ -2,8 +2,8 @@ if not game:IsLoaded() then
     game.Loaded:Wait()
 end
 
--- Wait 15 seconds to ensure UE Ragebot and other executors fully load and initialize their camera hooks before scar.lol loads
-task.wait(15)
+-- Wait 3 seconds to ensure UE Ragebot and other executors fully load and initialize their camera hooks before scar.lol loads
+task.wait(3)
 local CoreGui = game:GetService("CoreGui")
 local Players = game:GetService("Players")
 local rs = game:GetService("RunService")
